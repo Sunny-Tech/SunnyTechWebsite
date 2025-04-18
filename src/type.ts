933a7996128c5470faddc5f77c3750749252624b
Thomas Paillot <thomas.paillot@inria.fr>
2025-04-18 23:46:03 +0200

    Add schedule and sessions pages

--- src/type.ts
+++ src/type.ts
@@ -1,4 +1,8 @@
 export type OpenPlannerType = {
+    event: {
+        tracks: Track[]
+        categories: Category[]
+    }
     sponsors: {
         name: string
         order: number
@@ -8,6 +12,7 @@ export type OpenPlannerType = {
             logoUrl: string
         }[]
     }[]
+    sessions: Session[]
     team: {
         name: string
         photoUrl: string
@@ -17,8 +22,53 @@ export type OpenPlannerType = {
             link: string
         }[]
     }[]
-    speakers: {
-        name: string
-        id: string
-    }[]
+    speakers: Speaker[]
+}
+
+export interface Track {
+    id: string
+    name: string
+}
+
+export interface Category {
+    id: string
+    name: string
+    color?: string
+    colorSecondary?: string
+}
+
+export interface Session {
+    id: string
+    title: string
+    abstract: string | null
+    dateStart: string | null | undefined
+    dateEnd: string | null | undefined
+    durationMinutes: number
+    speakerIds: string[]
+    trackId: string | null
+    language: string | null
+    level: string | null
+    imageUrl: string | null | undefined
+    presentationLink: string | null | undefined
+    videoLink: string | null | undefined
+    tags: string[]
+    formatId: string | null
+    categoryId: string | null
+    showInFeedback: boolean
+    hideTrackTitle: boolean
+    extendHeight: number | undefined
+    extendWidth: number | undefined
+    teaserVideoUrl: string | null | undefined
+    teaserImageUrl: string | null | undefined
+}
+
+export interface Speaker {
+    id: string
+    name: string
+    pronouns: string | null | undefined
+    jobTitle: string | null | undefined
+    bio: string | null
+    company: string | null | undefined
+    companyLogoUrl: string | null | undefined
+    photoUrl: string | null | undefined
 }
