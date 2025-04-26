1c879de72fff8d458be415fda23b9cdcd0a9cf3b
Thomas Paillot <paillot.thomas@gmail.com>
2025-04-26 14:40:51 +0200

    Add schedule page (#26)
    * Upgrade to Astro 5
    
    * Add schedule and sessions pages
    
    * Minor change to trigger rebuild
    
    * First step towards user schedule
    
    * Pass env var in github workflows
    
    * Fix issue with timezone
    
    * Responsive schedule
    
    * Increase speaker photo on session page
    
    * Speaker page
    
    * Fix issue with github action
    
    * Minor improvements
    
    * Add back to schedule link on session page
    
    * Only load firebase lib on schedule pages
    
    * Improves speaker page
    
    * Hide schedule link from main menu
    
    * Show weekday and end time on session detail page
    
    * Put the category at the bottom of the session box on the schedule page
    
    * Fix session back to schedule link
    
    ---------
    
    Co-authored-by: Thomas Paillot <thomas.paillot@inria.fr>
    Co-authored-by: Hugo Gresse <hugo.gresse@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sunny-tech.io',
     base: '/',
-    experimental: {
-        contentLayer: true,
+    redirects: {
+        '/schedule': '/schedule/day-1'
     },
+    env: {
+        schema: {
+            OPENPLANNER_URL: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_API_KEY: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_AUTH_DOMAIN: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_DATABASE_URL: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_PROJECT_ID: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_STORAGE_BUCKET: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_MESSAGING_SENDER_ID: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_APP_ID: envField.string({ context: 'client', access: 'public', optional: false }),
+            FIREBASE_MEASUREMENT_ID: envField.string({ context: 'client', access: 'public', optional: true }),
+        }
+    }
 })
